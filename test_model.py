f1ab4024557db021612f6b7b4099041941d6301b
Sahiti7 <kurellalaxmisahiti@gmail.com>
2026-03-11 14:34:34 +0530

    fix project structure

--- test_model.py
+++ test_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
-X=np.array([1],[2],[3],[4])
+X = np.array([[1],[2],[3],[4]])
 Y=np.array([2,4,6,8])
 def test_coefficient():
     model=LinearRegression()
